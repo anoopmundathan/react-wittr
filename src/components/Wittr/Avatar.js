25dd470ff8b861e934e7e2e8fbed50f9a1082606
anoopmundathan <anoop.mundathan@gmail.com>
2017-06-02 21:18:35 +0100

    Remove Avatar component

--- src/components/Wittr/Avatar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Avatar = (props) => {
-	return(
-		<img src={props.avatar} alt="" className="post-avatar" />
-	);
-}
-
-export default Avatar;
--- src/components/Wittr/Main.js
+++ src/components/Wittr/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Article from './Article';
 
 export const Main = (props) => {	
-	console.log(props.posts);
 	return(
     	<main className="main">
     		{props.posts.map(post => <Article post={post}/>)}
